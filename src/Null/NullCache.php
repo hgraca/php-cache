1a2bf6655cfe77880747c0e2f18c47e863efa9b7
Herberto Graca <herberto.graca@gmail.com>
2016-12-10 15:27:20 +0100

    improvements of  NullCache

--- src/Null/NullCache.php
+++ src/Null/NullCache.php
@@ -2,14 +2,26 @@
 
 namespace Hgraca\Cache\Null;
 
+use DateTime;
 use Hgraca\Cache\CacheInterface;
 use Hgraca\Cache\Exception\CacheItemNotFoundException;
 
 final class NullCache implements CacheInterface
 {
+    /** @var DateTime */
+    private $creationTime;
+
+    /** @var int */
+    private $misses;
+
+    public function __construct()
+    {
+        $this->creationTime = new DateTime();
+    }
 
     public function fetch(string $id)
     {
+        $this->misses++;
         throw new CacheItemNotFoundException();
     }
 
@@ -32,8 +44,8 @@ final class NullCache implements CacheInterface
     {
         return [
             static::STATS_HITS => 0,
-            static::STATS_MISSES => 0,
-            static::STATS_UPTIME => 0,
+            static::STATS_MISSES => $this->misses,
+            static::STATS_UPTIME => $this->creationTime->getTimestamp(),
             static::STATS_MEMORY_USAGE => memory_get_usage(),
             static::STATS_MEMORY_AVAILABLE => ini_get('memory_limit') - memory_get_usage(),
             static::STATS_ITEM_COUNT => 0,
